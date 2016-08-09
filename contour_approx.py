ca4161aa21e47ecc3bc851dace8498d6829488d1
krishnac7 <kckckrishnachaITANYA7@gmail.com>
2016-08-09 18:36:18 +0530

    end of the day commit

--- contour_approx.py
+++ contour_approx.py
@@ -1,9 +1,11 @@
 import cv2
 
-img=imread("mad_square.jpg")
+img=cv2.imread("Lenna.png",0)
 ret,thresh=cv2.threshold(img,127,255,0)
-_,contours,hierarchy=cv2.findContours(thresh,1,2)\
-cnt=contours[0]
-epsilon=0.1*cv2.arcLength(cnt,true)
-approx=cv2.approxPolyDp(cnt,epsilon,True)
-cv2.drawContours
+_,contour,hierarchy=cv2.findContours(img.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+cnt=contour[0]
+epsilon=0.1*cv2.arcLength(cnt,True)
+approx=cv2.approxPolyDP(cnt,epsilon,True)
+cv2.drawContours(img, approx, -1, (0,255,130), 3)
+cv2.imshow("approx",img)
+cv2.waitKey(0)
